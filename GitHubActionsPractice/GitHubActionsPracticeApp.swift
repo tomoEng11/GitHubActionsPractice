2881c4a349c6955f7b2ea0c25de662bb41433b90
tomoEng11 <tom_friendship@me.com>
2024-09-20 21:05:23 +0900

    Initial Commit

---
+++ GitHubActionsPractice/GitHubActionsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsPracticeApp.swift
+//  GitHubActionsPractice
+//
+//  Created by 井本智博 on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
